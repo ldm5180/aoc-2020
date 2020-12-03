30a06d43d9de48a9f7e4f269dc766da86280fbd1
Lenny Maiorani <lenny@colorado.edu>
2020-12-02 17:29:03 -0700

    make the build script a little more flexible

--- build-it.sh
+++ build-it.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for f in *.cpp; do
+files=${1:-"*.cpp"}
+
+for f in $files; do
     ccache clang++-12 -std=c++20 -O3 \
 	   -Werror -Wall -Wextra -pedantic \
 	   -fconstexpr-steps=1000000000 \
