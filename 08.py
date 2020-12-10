c601e03314df4f0623b9abdd8d128f2231635ecf
Lenny Maiorani <lenny@colorado.edu>
2020-12-09 19:37:45 -0700

    Day 8, part 2

--- 08.py
+++ 08.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 #!/usr/bin/env python3
 
 import os
@@ -17,24 +15,45 @@ def read_input(filename):
 
 def handle(instruction):
     if instruction.startswith('acc'):
-        return int(instruction[4:]), 1
+        return int(instruction[4:]), 1, False, instruction
     elif instruction.startswith('jmp'):
-        return 0, int(instruction[4:])
+        return 0, int(instruction[4:]), True, instruction
 
-    return 0, 1
+    return 0, 1, True, instruction
 
-def process(instructions):
+def modify_instruction(inst):
+    if inst.startswith('nop'):
+        return 'jmp' + inst[3:]
+    elif inst.startswith('jmp'):
+        return 'nop' + inst[3:]
+    return inst
+
+def process(instructions, iteration):
     run = []
     accum = 0
     pc = 0
-    while(pc not in run):
-        add, adv = handle(instructions[pc])
+    while(pc not in run and pc != len(instructions)):
+        add, adv, is_suspect, inst_run = handle(instructions[pc])
+        if is_suspect and iteration == 0:
+            add, adv, is_suspect, inst_run = handle(modify_instruction(instructions[pc]))
+            iteration -= 1
+        elif is_suspect:
+            iteration -= 1
         run.append(pc)
         accum += add
         pc += adv
+    return accum, pc
+
+def search(instructions):
+    pc = 0
+    accum = 0
+    iteration = 0
+    while(pc != len(instructions)):
+        accum, pc = process(instructions, iteration)
+        iteration += 1
     return accum
 
 if __name__ == "__main__":
     input_file = get_input_file();
     instructions = read_input(input_file)
-    print("08 ---> " + str(process(instructions)))
+    print("08 ---> " + str(search(instructions)))
