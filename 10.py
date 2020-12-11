35e2aabf7fa28327b1e2ca5253f45589bfbddf23
Lenny Maiorani <lenny@colorado.edu>
2020-12-10 18:12:17 -0700

    Day 10, part 2

--- 10.py
+++ 10.py
@@ -24,7 +24,35 @@ def answer(diffs):
     threes = diffs.count(3) + 1
     return ones * threes
 
+def run_math(diffs):
+    runs_of_5 = 0
+    runs_of_4 = 0
+    runs_of_3 = 0
+    runs_of_2 = 0
+    while len(diffs) > 1:
+        s5 = diffs[:min([len(diffs), 5])]
+        s4 = diffs[:min([len(diffs), 4])]
+        s3 = diffs[:min([len(diffs), 3])]
+        s2 = diffs[:min([len(diffs), 2])]
+        if s5.count(1) == 5:
+            runs_of_5 += 1
+            diffs = diffs[5:]
+        if s4.count(1) == 4:
+            runs_of_4 += 1
+            diffs = diffs[4:]
+        elif s3.count(1) == 3:
+            runs_of_3 += 1
+            diffs = diffs[3:]
+        elif s2.count(1) == 2:
+            runs_of_2 += 1
+            diffs = diffs[2:]
+        else:
+            diffs = diffs[1:]
+    return (2**runs_of_2) * (4**runs_of_3) * (7**runs_of_4)
+
 if __name__ == "__main__":
     input = [0] + read_input(get_input_file())
     diffs = adjacent_diffs(input)
     print("10 ---> " + str(answer(diffs)))
+    part2 = run_math(diffs)
+    print("part 2 ---> " + str(part2))
