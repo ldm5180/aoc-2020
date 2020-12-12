ddeeee3d52b1726f0c4fc592759eb96ec2f9a2d0
Lenny Maiorani <lenny@colorado.edu>
2020-12-11 18:56:07 -0700

    Day 11, part 2

--- 11.py
+++ 11.py
@@ -14,13 +14,25 @@ def read_input(filename):
             data[i] = list(line.strip())
         return data
 
+def is_filled(input, r, c, next_row, next_col):
+    while(True):
+        r += next_row
+        c += next_col
+        if r >= 0 and r < len(input) and c >= 0 and c < get_line_len(input):
+            if input[r][c] == 'L':
+                return False
+            elif input[r][c] == '#':
+                return True
+        else:
+            return False
+
 def get_line_len(input):
     return len(input[0])
 
-def get_num_adjacent_filled(row_num, col_num, input):
-    neighbors = [ (row_num - 1, col_num - 1), (row_num - 1, col_num), (row_num - 1, col_num + 1),
-                  (row_num, col_num - 1),                         (row_num, col_num + 1),
-                  (row_num + 1, col_num - 1), (row_num + 1, col_num), (row_num + 1, col_num + 1) ]
+def get_num_adjacent_filled(row, col, input):
+    neighbors = [ (row - 1, col - 1), (row - 1, col), (row - 1, col + 1),
+                  (row, col - 1),                         (row, col + 1),
+                  (row + 1, col - 1), (row + 1, col), (row + 1, col + 1) ]
     count = 0
     for r,c in neighbors:
         if r >= 0 and r < len(input) and c >= 0 and c < get_line_len(input):
@@ -30,16 +42,32 @@ def get_num_adjacent_filled(row_num, col_num, input):
                 count += 1
     return count
 
-def fill_seats(input):
+def get_num_seen_filled(row, col, input):
+    count = 0
+    count += is_filled(input, row, col, -1, -1)
+    count += is_filled(input, row, col, -1, 0)
+    count += is_filled(input, row, col, -1, 1)
+    count += is_filled(input, row, col, 0, -1)
+    count += is_filled(input, row, col, 0, 1)
+    count += is_filled(input, row, col, 1, -1)
+    count += is_filled(input, row, col, 1, 0)
+    count += is_filled(input, row, col, 1, 1)
+    return count
+
+def fill_seats(input, look_deeply = False, num_next_to = 4):
     is_changed = False
     output = deepcopy(input)
     for row_num, row in enumerate(input):
         for col_num, seat in enumerate(row):
-            num_adjacent_filled = get_num_adjacent_filled(row_num, col_num, input)
+            if not look_deeply:
+                num_adjacent_filled = get_num_adjacent_filled(row_num, col_num, input)
+            else:
+                num_adjacent_filled = get_num_seen_filled(row_num, col_num, input)
+
             if num_adjacent_filled == 0 and seat == 'L':
                 output[row_num][col_num] = '#'
                 is_changed = True
-            elif num_adjacent_filled >= 4 and seat == '#':
+            elif num_adjacent_filled >= num_next_to and seat == '#':
                 output[row_num][col_num] = 'L'
                 is_changed = True
     return is_changed, output
@@ -53,15 +81,23 @@ def count_filled(input):
 def part1():
     input = read_input(get_input_file())
     row_len = get_line_len(input)
-    while (is_changed):
+    while (True):
         is_changed, input = fill_seats(input)
         if not is_changed:
             break
     filled = count_filled(input)
-    print("11 ---> " + str(filled))
+    print("11.1 ---> " + str(filled))
 
 def part2():
-    pass
+    input = read_input(get_input_file())
+    row_len = get_line_len(input)
+    while (True):
+        is_changed, input = fill_seats(input, True, 5)
+        if not is_changed:
+            break
+    filled = count_filled(input)
+    print("11.2 ---> " + str(filled))
+
 
 if __name__ == "__main__":
     part1()
